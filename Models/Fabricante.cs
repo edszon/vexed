892888de69356adada5dadaca769b509dcf50167
Joanderfilho11 <joanderson.santos7@gmail.com>
2021-11-30 16:10:57 -0300

    Classe Grupo e Fabricante

---
+++ Models/Fabricante.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace vexed.Models
+{
+    public class Fabricante
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdFabricante { get; set; }
+
+        [Required]
+        [MaxLength(32)]
+        public string Fabricante { get; set; }
+    }
+}
---
+++ Models/Grupo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace vexed.Models
+{
+    public class Grupo
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdGrupo { get; set; }
+
+        [Required]
+        [MaxLength(32)]
+        public string Grupo { get; set; }
+    }
+}
